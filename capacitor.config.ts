1f676b17a9a80c32373e496b9befd3fc6da8d488
pwespi <pwespi@pm.me>
2021-05-23 21:42:03 +0200

    migrate to typescript config

---
+++ capacitor.config.ts
@@ -0,0 +1,28 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.wespiapps.bluetoothle.example",
+  appName: "BLE Example",
+  bundledWebRuntime: false,
+  webDir: "www",
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+    },
+    BluetoothLe: {
+      displayStrings: {
+        scanning: "Am Scannen...",
+        cancel: "Abbrechen",
+        availableDevices: "Verfügbare Geräte",
+        noDeviceFound: "Kein Gerät gefunden",
+      },
+    },
+  },
+  cordova: {},
+  server: {
+    url: "http://192.168.178.24:3333",
+    cleartext: true,
+  },
+};
+
+export default config;
